32d3a84ed5b47a43ce3c1bdce9666cb72952349d
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-07-13 16:58:26 -0300

    feat: criando meu pacote de entitades e minha classe todo

---
+++ src/main/java/com/hugodiniz/todolist/entity/Todo.java
@@ -0,0 +1,66 @@
+package com.hugodiniz.todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean status;
+    private TodoPriority priority;
+
+    public Todo() {
+    }
+
+    public Todo(Long id, String name, String description, boolean status, TodoPriority priority) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public TodoPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TodoPriority priority) {
+        this.priority = priority;
+    }
+}
