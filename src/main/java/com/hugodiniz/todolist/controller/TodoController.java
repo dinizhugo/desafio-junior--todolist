b9e4dc670da1e830a9f4ff08be0e78e4cda4f00c
Hugo-Diniz <hugodiniztech01@gmail.com>
2024-07-13 18:23:07 -0300

    feat: controller

--- src/main/java/com/hugodiniz/todolist/controller/TodoController.java
+++ src/main/java/com/hugodiniz/todolist/controller/TodoController.java
@@ -1,18 +1,26 @@
 package com.hugodiniz.todolist.controller;
 
 import com.hugodiniz.todolist.dto.TodoRequest;
+import com.hugodiniz.todolist.entity.Todo;
+import com.hugodiniz.todolist.service.TodoService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-    @RequestMapping("/todo")
-    public class TodoController {
+@RequestMapping("/todo")
+public class TodoController {
 
-        @PostMapping
-        public ResponseEntity<?> createNewTodo(@Valid TodoRequest data) {
+    @Autowired
+    private TodoService service;
 
-        }
+    @PostMapping
+    public ResponseEntity<Todo> createNewTodo(@Valid TodoRequest data) {
+        var obj = service.createNewTodo(data);
+        return ResponseEntity.status(HttpStatus.CREATED).body(obj);
+    }
 }
